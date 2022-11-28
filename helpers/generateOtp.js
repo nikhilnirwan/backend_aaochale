e3701a4c254b02db7397aaea50d38990a8ce6d7c
nikhil <nikhil.nirwan@startlazaa.com>
2022-11-28 19:23:24 +0530

    add

--- helpers/generateOtp.js
+++ helpers/generateOtp.js
@@ -1,4 +1,5 @@
 const path = require("path");
+require("dotenv").config();
 // const axios = require("axios");
 const twilio = require("twilio")(process.env.TWILIO_ACC_SID, process.env.TWILIO_AUTH_TOKEN);
 const Email = require(path.join(__dirname, "..", "utils", "Email"));
