c8bdfc4a338965d07a2ff0730a658478da157269
nikhil <nikhil.nirwan@startlazaa.com>
2022-11-28 20:13:18 +0530

    add

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const DB_URL = "mongodb+srv://AaooChale:DL11P7C5U22YSBzB@aaoochaledb.wxcfmcd.mongodb.net/aaoochale";
 const dbConnect = () => {
   mongoose
     .connect(process.env.DB_URL)
